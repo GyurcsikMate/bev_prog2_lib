8c35be4aff50d296c9356333645a348a8320dc02
GyurcsikMate <65111077+GyurcsikMate@users.noreply.github.com>
2020-06-20 21:33:02 +0200

    lefordul, itt vannak a widget leírások
    lefordul, itt vannak a widget leírások

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include "graphics.hpp"
-#include "widgetss.hpp"
-#include "alkalkm.hpp"
-#include "gorgos.hpp"
-#include "statikusszovegdoboz.hpp"
 #include "szovegdobozosapp.hpp"
 using namespace std;
 using namespace genv;
 
-
+/// gorgos.hpp - Gorgi szambeallito valamilyen esemenyre(novel vagy csokkent)
+///muveletlista.hpp - Lista egy vectorban váltogat az elemek kozott
+//dinamo.hpp - Dinamik igazabol meg nem igazan jo, majd dinamikus szovegkezelo kene hogy legyen
+///dobi.hpp - Doboz igaz - hamis doboz
+///statikusszovegdoboz.hpp - Szovi ez tóegyértelmû
 int main()
 {
     gout.open(400,400);
+    event ev;
+    Szovegapp balta;
+    balta.draw();
 
+    while(gin>>ev)
+    {
 
-    Szovegapp egyenlorecsakstatik;
-    egyenlorecsakstatik.event_loop();
+    }
+    //Szovegapp egyenlorecsakstatik;
+    //egyenlorecsakstatik.event_loop(ev);
     return 0;
 }
